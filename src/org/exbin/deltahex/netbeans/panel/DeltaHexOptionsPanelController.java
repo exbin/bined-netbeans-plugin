d9bc5cdc76082ff1742f62ad831aec91c409b9b4
hajdam <hajdam@users.sf.net>
2017-02-28 21:56:25 +0100

    Added options panel - 1

---
+++ src/org/exbin/deltahex/netbeans/panel/DeltaHexOptionsPanelController.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * This application or library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This application or library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along this application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exbin.deltahex.netbeans.panel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+/**
+ * Hexadecimal editor options panel.
+ *
+ * @version 0.1.5 2017/02/28
+ * @author ExBin Project (http://exbin.org)
+ */
+@OptionsPanelController.SubRegistration(
+        location = "Editor",
+        displayName = "#AdvancedOption_DisplayName_DeltaHex",
+        keywords = "#AdvancedOption_Keywords_DeltaHex",
+        keywordsCategory = "Editor/DeltaHex"
+)
+@org.openide.util.NbBundle.Messages({"AdvancedOption_DisplayName_DeltaHex=DeltaHex", "AdvancedOption_Keywords_DeltaHex=deltahex hexadecimal"})
+public final class DeltaHexOptionsPanelController extends OptionsPanelController {
+
+    private DeltaHexOptionsPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                getPanel().store();
+                changed = false;
+            }
+        });
+    }
+
+    @Override
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private DeltaHexOptionsPanel getPanel() {
+        if (panel == null) {
+            panel = new DeltaHexOptionsPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
