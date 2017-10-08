2c0498f522d010e7e367d3317bc898d365c0ea06
hajdam <hajdam@users.sf.net>
2017-10-08 23:34:21 +0200

    Fixing charset translation - 3

--- src/org/exbin/deltahex/operation/swing/CodeAreaOperationCommandHandler.java
+++ src/org/exbin/deltahex/operation/swing/CodeAreaOperationCommandHandler.java
@@ -790,11 +790,13 @@ public class CodeAreaOperationCommandHandler implements CodeAreaCommandHandler {
                     long clipDataSize = insertedData.getDataSize();
                     long insertionPosition = dataPosition;
                     if (codeArea.getEditationMode() == EditationMode.OVERWRITE) {
-                        BinaryData modifiedData = insertedData;
+                        BinaryData modifiedData;
                         long replacedPartSize = clipDataSize;
                         if (insertionPosition + replacedPartSize > dataSize) {
                             replacedPartSize = dataSize - insertionPosition;
                             modifiedData = insertedData.copy(0, replacedPartSize);
+                        } else {
+                            modifiedData = insertedData.copy();
                         }
                         if (replacedPartSize > 0) {
                             modifyCommand = new ModifyDataCommand(codeArea, dataPosition, modifiedData);
@@ -808,7 +810,7 @@ public class CodeAreaOperationCommandHandler implements CodeAreaCommandHandler {
                     }
 
                     CodeAreaCommand insertCommand = null;
-                    if (clipDataSize > 0) {
+                    if (!insertedData.isEmpty()) {
                         insertCommand = new InsertDataCommand(codeArea, insertionPosition, insertedData);
                     }
 
