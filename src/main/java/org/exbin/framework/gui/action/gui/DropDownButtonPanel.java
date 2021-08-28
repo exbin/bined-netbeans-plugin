0462ca214d860280af478af13df62260adf8394b
hajdam <hajdam@users.sf.net>
2021-08-28 22:55:27 +0200

    Minor fixes - 35

--- src/main/java/org/exbin/framework/gui/action/gui/DropDownButtonPanel.java
+++ src/main/java/org/exbin/framework/gui/action/gui/DropDownButtonPanel.java
@@ -23,13 +23,14 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.basic.BasicArrowButton;
 import org.exbin.framework.gui.utils.WindowUtils;
 
 /**
  * Drop down button panel.
  *
- * @version 0.2.1 2019/08/17
+ * @version 0.2.1 2021/08/28
  * @author ExBin Project (http://exbin.org)
  */
 @ParametersAreNonnullByDefault
@@ -65,15 +66,9 @@ public class DropDownButtonPanel extends javax.swing.JPanel {
             }
 
             private void passMouseEventToParent(MouseEvent me) {
-                Component child = me.getComponent();
                 Component parent = DropDownButtonPanel.this.getParent();
-
-                //transform the mouse coordinate to be relative to the parent component:
-                int deltaX = child.getX() + me.getX();
-                int deltaY = child.getY() + me.getY();
-
                 //dispatch it to the parent component
-                parent.dispatchEvent(new MouseEvent(parent, me.getID(), me.getWhen(), me.getModifiersEx(), deltaX, deltaY, me.getClickCount(), false));
+                parent.dispatchEvent(SwingUtilities.convertMouseEvent(actionLabel, me, parent));
             }
         });
     }
@@ -100,6 +95,7 @@ public class DropDownButtonPanel extends javax.swing.JPanel {
         menuButton = new BasicArrowButton(SwingConstants.SOUTH);
         actionLabel = new javax.swing.JLabel();
 
+        setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
         setFocusable(false);
         setOpaque(false);
 
